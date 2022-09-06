22954c128cd2357dcb2b23321f71b00bc12d0678
Usman-47 <usmanjamil047@gmail.com>
2022-09-06 16:54:45 +0500

    working

--- client/src/pages/private/CRUD/Invoice/ReadOnePool.js
+++ client/src/pages/private/CRUD/Invoice/ReadOnePool.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
+import { connect } from "react-redux";
 
 import { useWallet, useConnection } from "@solana/wallet-adapter-react";
 import {
@@ -34,12 +35,15 @@ import useStatesFunc from "../../../../hooks/useStatesFunc";
 import IDL from "../../components/twitter_program.json";
 import IDLFORRAID from "../../components/twitter_program_for_raid.json";
 
-const ReadOneInvoice = () => {
+const ReadOneInvoice = ({ auth }) => {
   const { wallet, connect, sendTransaction, connecting, publicKey } =
     useWallet();
   const { connection } = useConnection();
 
   const [isRaid, setIsRaid] = useState();
+  const [splTokenForFundPool, setSplTokenForFundPool] = useState();
+  const [projectName, setProjectName] = useState();
+  const [amount, setAmount] = useState(1);
 
   const [dispatch] = useDispatchFunc();
   const [{ loading }] = useStatesFunc();
@@ -79,6 +83,8 @@ const ReadOneInvoice = () => {
       // this is for Details esssential for invoice
       const { data } = await InvoicePoolFetchDataFunc(id);
       setIsRaid(data?.invoiceFound[0]?.isRaid);
+      setProjectName(data?.invoiceFound[0]?.projectName);
+      setSplTokenForFundPool(data?.invoiceFound[0]?.pool[0]?.splToken);
       dispatch({ type: "loadingStop" });
       if (data.type === "success") {
         toast.success(data.msg);
@@ -90,10 +96,8 @@ const ReadOneInvoice = () => {
     getinvoiceData();
   }, [dispatch, id, navigate]);
 
-  const fundUserPool = async () => {
-    const mintAddress = new PublicKey(
-      "3pCLx1uK3PVFGQ3siyxurvXXSLijth2prgBEK4cS33XF"
-    );
+  const fundUserPoolForRaid = async () => {
+    const mintAddress = new PublicKey(splTokenForFundPool);
     if (publicKey) {
       const [globalAuth, globalBump] =
         await anchor.web3.PublicKey.findProgramAddress(
@@ -152,6 +156,275 @@ const ReadOneInvoice = () => {
     }
   };
 
+  const fundUserPool = async () => {
+    const mintAddress = new PublicKey(splTokenForFundPool);
+    // const mintAddress = new PublicKey("3pCLx1uK3PVFGQ3siyxurvXXSLijth2prgBEK4cS33XF")
+    if (publicKey) {
+      const [globalAuth, globalBump] =
+        await anchor.web3.PublicKey.findProgramAddress(
+          [Buffer.from("global-authority")],
+          // new anchor.BN(47).toArrayLike(Buffer)],
+          program.programId
+        );
+      console.log(globalAuth.toString(), "globalAuth");
+
+      const [poolAddress] = await anchor.web3.PublicKey.findProgramAddress(
+        [
+          anchor.utils.bytes.utf8.encode("pool"),
+          provider.wallet.publicKey.toBuffer(),
+          mintAddress.toBuffer(),
+          Buffer.from(projectName),
+        ],
+        program.programId
+      );
+      console.log(poolAddress.toString(), "poolAddress");
+
+      const [poolAta] = await anchor.web3.PublicKey.findProgramAddress(
+        [
+          anchor.utils.bytes.utf8.encode("poolAta"),
+          provider.wallet.publicKey.toBuffer(),
+          mintAddress.toBuffer(),
+          Buffer.from(projectName),
+        ],
+        program.programId
+      );
+
+      let clientAta = (
+        await PublicKey.findProgramAddress(
+          [
+            provider.wallet.publicKey.toBuffer(),
+            TOKEN_PROGRAM_ID.toBuffer(),
+            mintAddress.toBuffer(), // mint address
+          ],
+          ASSOCIATED_TOKEN_PROGRAM_ID
+        )
+      )[0];
+      console.log(clientAta.toString(), "clientAta");
+
+      const userAtaCheck = await solConnection.getTokenAccountsByOwner(
+        provider.wallet.publicKey,
+        { mint: mintAddress }
+      );
+
+      let instructions = [];
+      if (userAtaCheck.value.length === 0) {
+        if (NATIVE_MINT.toString() === mintAddress.toString()) {
+          instructions.push(
+            Token.createAssociatedTokenAccountInstruction(
+              ASSOCIATED_TOKEN_PROGRAM_ID,
+              TOKEN_PROGRAM_ID,
+              mintAddress,
+              clientAta,
+              provider.wallet.publicKey,
+              provider.wallet.publicKey
+            ),
+            // Token.createTransferInstruction(TOKEN_PROGRAM_ID, provider.wallet.publicKey, clientAta, provider.wallet.publicKey, [], 100000000)
+            SystemProgram.transfer({
+              fromPubkey: provider.wallet.publicKey,
+              toPubkey: clientAta,
+              lamports: 1000000000,
+            }),
+            // TOKEN_PROGRAM_ID, provider.wallet.publicKey, clientAta, provider.wallet.publicKey, [], 100000000
+            // await Token.createWrappedNativeAccount(solConnection, TOKEN_PROGRAM_ID, provider.wallet.publicKey, provider.wallet, 100000000)
+            // ,
+            Token.createSyncNativeInstruction(TOKEN_PROGRAM_ID, clientAta)
+          );
+          console.log("wrapped");
+        }
+      } else {
+        if (NATIVE_MINT.toString() === mintAddress.toString()) {
+          instructions.push(
+            // Token.createTransferInstruction(TOKEN_PROGRAM_ID, provider.wallet.publicKey, clientAta, provider.wallet.publicKey, [], 100000000)
+            SystemProgram.transfer({
+              fromPubkey: provider.wallet.publicKey,
+              toPubkey: clientAta,
+              lamports: 1000000000,
+            }),
+            // TOKEN_PROGRAM_ID, provider.wallet.publicKey, clientAta, provider.wallet.publicKey, [], 100000000
+            // await Token.createWrappedNativeAccount(solConnection, TOKEN_PROGRAM_ID, provider.wallet.publicKey, provider.wallet, 100000000)
+            // ,
+            Token.createSyncNativeInstruction(TOKEN_PROGRAM_ID, clientAta)
+          );
+        }
+      }
+
+      try {
+        const tx = await program.rpc.fundUserPool(
+          projectName,
+          new anchor.BN(10000000),
+          {
+            accounts: {
+              client: provider.wallet.publicKey,
+              pool: poolAddress,
+              poolAta: poolAta,
+              poolMint: mintAddress,
+              clientAta: clientAta,
+              tokenProgram: TOKEN_PROGRAM_ID,
+            },
+            instructions,
+          }
+        );
+        console.log(tx, "tx");
+      } catch (err) {
+        console.log(err, "err");
+      }
+    }
+  };
+
+  const updateUserPoolForRaid = async () => {
+    const mintAddress = new PublicKey(
+      "3pCLx1uK3PVFGQ3siyxurvXXSLijth2prgBEK4cS33XF"
+    );
+    const feeAcc = new PublicKey(
+      "7VPjjEj7mukgBf9TqpDxivnu7BNH4rdUmSFUpgvpLvf7"
+    );
+    const startTime = "1234";
+    const timeLimit = 59;
+    const isPaused = true;
+    const isCompleted = true;
+    const funds = 10000000000; // 10 SPL
+    if (publicKey) {
+      const [globalAuth, globalBump] =
+        await anchor.web3.PublicKey.findProgramAddress(
+          [Buffer.from("global-authority")],
+          // new anchor.BN(47).toArrayLike(Buffer)],
+          program.programId
+        );
+      console.log(globalAuth.toString(), "globalAuth");
+
+      const [poolAddress] = await anchor.web3.PublicKey.findProgramAddress(
+        [
+          anchor.utils.bytes.utf8.encode("pool"),
+          provider.wallet.publicKey.toBuffer(),
+          mintAddress.toBuffer(),
+          Buffer.from(projectName),
+        ],
+        program.programId
+      );
+      console.log(poolAddress.toString(), "poolAddress");
+
+      const clientAta = await solConnection.getTokenAccountsByOwner(
+        provider.wallet.publicKey,
+        { mint: mintAddress }
+      );
+      const [poolAta] = await anchor.web3.PublicKey.findProgramAddress(
+        [
+          anchor.utils.bytes.utf8.encode("poolAta"),
+          provider.wallet.publicKey.toBuffer(),
+          mintAddress.toBuffer(),
+          Buffer.from(projectName),
+        ],
+        program.programId
+      );
+      console.log(clientAta.value[0].pubkey.toString(), "clientAta");
+      console.log(poolAta.toString(), "poolAta");
+
+      try {
+        const tx = await program.rpc.updateUserPool(
+          projectName,
+          startTime,
+          new anchor.BN(timeLimit),
+          isPaused,
+          isCompleted,
+          {
+            accounts: {
+              client: provider.wallet.publicKey,
+              globalAuthority: globalAuth,
+              pool: poolAddress,
+              poolAta: poolAta,
+              poolMint: mintAddress,
+              feeAcc: feeAcc,
+              clientAta: clientAta.value[0].pubkey,
+              systemProgram: SystemProgram.programId,
+              tokenProgram: TOKEN_PROGRAM_ID,
+              rent: SYSVAR_RENT_PUBKEY,
+            },
+          }
+        );
+        console.log(tx, "tx");
+      } catch (err) {
+        console.log(err, "err");
+      }
+    }
+  };
+
+  const updateUserPool = async () => {
+    const mintAddress = new PublicKey(
+      "3pCLx1uK3PVFGQ3siyxurvXXSLijth2prgBEK4cS33XF"
+    );
+    const feeAcc = new PublicKey(
+      "7VPjjEj7mukgBf9TqpDxivnu7BNH4rdUmSFUpgvpLvf7"
+    );
+    const startTime = "1234";
+    const timeLimit = 59;
+    const isPaused = true;
+    const isCompleted = true;
+    const funds = 10000000000; // 10 SPL
+    if (publicKey) {
+      const [globalAuth, globalBump] =
+        await anchor.web3.PublicKey.findProgramAddress(
+          [Buffer.from("global-authority")],
+          // new anchor.BN(47).toArrayLike(Buffer)],
+          program.programId
+        );
+      console.log(globalAuth.toString(), "globalAuth");
+
+      const [poolAddress] = await anchor.web3.PublicKey.findProgramAddress(
+        [
+          anchor.utils.bytes.utf8.encode("pool"),
+          provider.wallet.publicKey.toBuffer(),
+          mintAddress.toBuffer(),
+          Buffer.from(projectName),
+        ],
+        program.programId
+      );
+      console.log(poolAddress.toString(), "poolAddress");
+
+      const clientAta = await solConnection.getTokenAccountsByOwner(
+        provider.wallet.publicKey,
+        { mint: mintAddress }
+      );
+      const [poolAta] = await anchor.web3.PublicKey.findProgramAddress(
+        [
+          anchor.utils.bytes.utf8.encode("poolAta"),
+          provider.wallet.publicKey.toBuffer(),
+          mintAddress.toBuffer(),
+          Buffer.from(projectName),
+        ],
+        program.programId
+      );
+      console.log(clientAta.value[0].pubkey.toString(), "clientAta");
+      console.log(poolAta.toString(), "poolAta");
+
+      try {
+        const tx = await program.rpc.updateUserPool(
+          projectName,
+          startTime,
+          new anchor.BN(timeLimit),
+          isPaused,
+          isCompleted,
+          {
+            accounts: {
+              client: provider.wallet.publicKey,
+              globalAuthority: globalAuth,
+              pool: poolAddress,
+              poolAta: poolAta,
+              poolMint: mintAddress,
+              feeAcc: feeAcc,
+              clientAta: clientAta.value[0].pubkey,
+              systemProgram: SystemProgram.programId,
+              tokenProgram: TOKEN_PROGRAM_ID,
+              rent: SYSVAR_RENT_PUBKEY,
+            },
+          }
+        );
+        console.log(tx, "tx");
+      } catch (err) {
+        console.log(err, "err");
+      }
+    }
+  };
+
   if (loading) {
     return (
       <>
@@ -186,7 +459,26 @@ const ReadOneInvoice = () => {
               <div className="col-8 col-md-6 m-3">
                 <button
                   className="btn btn-info border-info text-white w-100 my-2"
-                  onClick={true}
+                  onClick={fundUserPoolForRaid}
+                >
+                  Subcription
+                </button>
+              </div>
+            ) : (
+              <div className="col-8 col-md-6 m-3">
+                <button
+                  className="btn btn-info border-info text-white w-100 my-2"
+                  onClick={fundUserPool}
+                >
+                  Subcription
+                </button>
+              </div>
+            )}
+            {isRaid ? (
+              <div className="col-8 col-md-6 m-3">
+                <button
+                  className="btn btn-info border-info text-white w-100 my-2"
+                  onClick={fundUserPoolForRaid}
                 >
                   Fund Pool
                 </button>
@@ -195,7 +487,7 @@ const ReadOneInvoice = () => {
               <div className="col-8 col-md-6 m-3">
                 <button
                   className="btn btn-info border-info text-white w-100 my-2"
-                  onClick={true}
+                  onClick={fundUserPool}
                 >
                   Fund Pool
                 </button>
@@ -227,5 +519,7 @@ const ReadOneInvoice = () => {
     </div>
   );
 };
-
-export default ReadOneInvoice;
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+export default connect(mapStateToProps)(ReadOneInvoice);
