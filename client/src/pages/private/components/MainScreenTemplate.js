9611390341b17c2b84d3193e530518671ad7122e
noumanazeem <noumanazeem1@gmail.com>
2022-09-09 21:48:12 +0500

    work on manager side

--- client/src/pages/private/components/MainScreenTemplate.js
+++ client/src/pages/private/components/MainScreenTemplate.js
@@ -4,16 +4,17 @@ const MainScreenTemplate = ({ role }) => {
   const [count, setCount] = useState();
   return (
     <>
-      <div className="col container p-md-5 py-2 my-5 border border-1 rounded-3" style={{background:"#333333",}}>
+      <div className="col container p-md-5 py-2 my-5 border border-1 " style={{background:"#333333",borderRadius:"30px"}}>
         <div className="p-4" style={{background:"#333333"}}>
-          <div className="display-2 my-3 p-md-3 col text-white text-center welcome_admin">
-            Welcome {role.toUpperCase()}
+          <div className="display-4 my-3 p-md-3 col text-white text-center welcome_admin">
+            {/* Welcome {role.toUpperCase()} */}
+            Coming Soon...
           </div>
-          <div className="col my-2 p-md-3 mx-auto d-flex justify-content-center ">
-            <div className="p-md-5 p-4 bg-black border border-1 rounded-3 text-center text-light mx-auto shadow">
+          {/* <div className="col my-2 p-md-3 mx-auto d-flex justify-content-center " style={{borderRadius:"30px"}}>
+            <div className="p-md-5 p-4 bg-black border border-1  text-center text-light mx-auto shadow border-5"  >
               <div className="fw-bolder  lead">Total Tweets : {count} </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
